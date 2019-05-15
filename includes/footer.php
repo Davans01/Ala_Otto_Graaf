be668d8670a598c83b1209ea7d98c384c9e0dc33
Davans01 <Daniel@maikelvanstraten.nl>
2019-05-15 13:32:51 +0200

    I dont know what i added

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+     <h2>&copy; Quins Maduro, daniel van straten, kevin</h2>
+</footer>
--- index.php
+++ index.php
@@ -3,14 +3,14 @@
     <head>
       <title>portfolio</title>
        <meta charset="UTF-8">
-      <link rel="stylesheet" href="otto.css">
+      <link rel="stylesheet" href="css/otto.css">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       </head>
       <body>
       <div class="bg-img">
         <div class="container">
           <div class="topnav">
-              <img class="logo" src="logo.png">
+              <img class="logo" src="images/logo.png">
             <a href="#news">Video</a>
             <a href="#contact">Foto</a>
             <a href="#about">Portfolio</a>
@@ -22,33 +22,23 @@
         <i class="fa fa-th btn-gallery active"></i>
         <i class="fa fa-list btn-list"></i>
       </nav>
-      
 
+<br><br>
       <div class="gallery">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/BUQU9ERJAC.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/L2XY1E8DEV.jpg">
-        <img class="picture" src="http://media.digitalcameraworld.com/wp-content/uploads/sites/123/2012/05/Landscape_photography_tips.foamy_3.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/DH3TS2T6NF.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/FVIYKRN6CI.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/J2XAUBP5Y1.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8Z2QWLCH72.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/BUQU9ERJAC.jpg">
-        <img class="picture" src="anton-repponen-99530-unsplash.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/BUQU9ERJAC.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/L2XY1E8DEV.jpg">
-        <img class="picture" src="http://media.digitalcameraworld.com/wp-content/uploads/sites/123/2012/05/Landscape_photography_tips.foamy_3.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/DH3TS2T6NF.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/FVIYKRN6CI.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/J2XAUBP5Y1.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8Z2QWLCH72.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/BUQU9ERJAC.jpg">
-        <img class="picture" src="anton-repponen-99530-unsplash.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/DH3TS2T6NF.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/FVIYKRN6CI.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/J2XAUBP5Y1.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8Z2QWLCH72.jpg">
-        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/BUQU9ERJAC.jpg">
-        <img class="picture" src="anton-repponen-99530-unsplash.jpg">
+        <img class="picture" src="images/chi-liu-1552956-unsplash.jpg">
+        <img class="picture" src="images/benjamin-voros-575800-unsplash.jpg">
+        <img class="picture" src="images/dimitry-anikin-1461827-unsplash.jpg">
+        <img class="picture" src="images/dominic-krainer-1381989-unsplash.jpg">
+        <img class="picture" src="images/erzsebet-vehofsics-1401802-unsplash.jpg">
+        <img class="picture" src="images/sebastien-jermer-27003-unsplash.jpg">
+        <img class="picture" src="images/jordan-arnold-1513926-unsplash.jpg">
+        <img class="picture" src="images/michael-liao-1508603-unsplash.jpg">
+        <img class="picture" src="images/mark-mathew-1289706-unsplash.jpg">
+        <img class="picture" src="images/pablo-guerrero-1308486-unsplash.jpg">
+        <img class="picture" src="images/luca-micheli-455516-unsplash.jpg">
+        <img class="picture" src="images/maarten-deckers-234714-unsplash.jpg">
+        <img class="picture" src="images/oliver-ash-1279414-unsplash.jpg">
+        <img class="picture" src="images/daniel-ferrandiz-mont-1316873-unsplash.jpg">
 
       </div>
       <script>
@@ -60,9 +50,8 @@ $(".picture").mouseleave(function(){
  $(this).animate({opacity:0.8},200);
 });
       </script>
-      </body>
-     <footer>
-          <h2>&copy; Quins Maduro, daniel van straten, kevin</h2>
-     </footer>
+      <?php
+      include("includes/footer.php");
+      ?>
       </body>
      </html>
