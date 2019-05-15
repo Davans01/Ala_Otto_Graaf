5fbc0b8884952f08213ec7a61960f1bbe92c71af
quinsley-maduro <48790001+quinsley-maduro@users.noreply.github.com>
2019-05-15 09:22:29 +0200

    Css added
    Added the css

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+  <html lang="nl">
+    <head>
+      <title>portfolio</title>
+       <meta charset="UTF-8">
+      <link rel="stylesheet" href="otto.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      </head>
+      <body>
+      <div class="bg-img">
+        <div class="container">
+          <div class="topnav">
+              <img class="logo" src="logo.png">
+            <a href="#news">Video</a>
+            <a href="#contact">Foto</a>
+            <a href="#about">Portfolio</a>
+            <a class="active" href="#about">Home</a>
+          </div>
+        </div>
+      </div>
+      <nav class="product-view">
+        <i class="fa fa-th btn-gallery active"></i>
+        <i class="fa fa-list btn-list"></i>
+      </nav>
+      
+
+      <div class="gallery">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/BUQU9ERJAC.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/L2XY1E8DEV.jpg">
+        <img class="picture" src="http://media.digitalcameraworld.com/wp-content/uploads/sites/123/2012/05/Landscape_photography_tips.foamy_3.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/DH3TS2T6NF.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/FVIYKRN6CI.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/J2XAUBP5Y1.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8Z2QWLCH72.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/BUQU9ERJAC.jpg">
+        <img class="picture" src="anton-repponen-99530-unsplash.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/BUQU9ERJAC.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/L2XY1E8DEV.jpg">
+        <img class="picture" src="http://media.digitalcameraworld.com/wp-content/uploads/sites/123/2012/05/Landscape_photography_tips.foamy_3.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/DH3TS2T6NF.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/FVIYKRN6CI.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/J2XAUBP5Y1.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8Z2QWLCH72.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/BUQU9ERJAC.jpg">
+        <img class="picture" src="anton-repponen-99530-unsplash.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/DH3TS2T6NF.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/FVIYKRN6CI.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/J2XAUBP5Y1.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8Z2QWLCH72.jpg">
+        <img class="picture" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/BUQU9ERJAC.jpg">
+        <img class="picture" src="anton-repponen-99530-unsplash.jpg">
+
+      </div>
+      <script>
+$(".picture").mouseenter(function(){
+ $(this).animate({opacity:1},200);
+});
+
+$(".picture").mouseleave(function(){
+ $(this).animate({opacity:0.8},200);
+});
+      </script>
+      </body>
+     <footer>
+          <h2>&copy; Quins Maduro, daniel van straten, kevin</h2>
+     </footer>
+      </body>
+     </html>
