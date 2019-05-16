ddc518d98dee8c6f3332568db2670eb9f78cdfff
Davans01 <Daniel@maikelvanstraten.nl>
2019-05-16 23:15:14 +0200

    Added some funtions

--- lib/lib.php
+++ lib/lib.php
@@ -1,8 +1,29 @@
 <?php
 
-function get_all_products() 
+function get_all_products()
 {
     global $db;
-    
+
+    return $db->get('products');
+}
+
+function get_all_nieuws()
+{
+    global $db;
+    $db->where("catogory_id = 1");
+    return $db->get('products');
+}
+
+function get_all_modellen()
+{
+    global $db;
+    $db->where("catogory_id = 2");
+    return $db->get('products');
+}
+
+function get_all_landen()
+{
+    global $db;
+    $db->where("catogory_id = 3");
     return $db->get('products');
 }
