434770899ce1a4cc5578e57ec2cfb95b193d9ce6
Maikel van Straten <m.vanstraten@avetica.nl>
2019-05-17 16:31:15 +0200

    order page

--- bestellen.php
+++ bestellen.php
@@ -10,7 +10,8 @@
         <!-- include header.php -->
         <?php include('includes/header.php') ?>
 
-
+        <h1>Beste bezoeker, 
+        <br> Dank u wel voor uw bestelling u kunt het geld over maken naar het banknummer: ING INGB 1234 2342 12</h1>
         <!-- Include footer.php -->
         <?php include('includes/footer.php') ?>
     </body>
